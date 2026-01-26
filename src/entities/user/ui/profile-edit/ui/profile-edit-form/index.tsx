a145ebc3eba1120a4c0e71f57f3af9756f8514c4
ondren <andreykl123321@gmail.com>
2026-01-26 16:26:49 +0300

    fix: error

--- src/entities/user/ui/profile-edit/ui/profile-edit-form/index.tsx
+++ src/entities/user/ui/profile-edit/ui/profile-edit-form/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import { FormRoot } from 'src/shared/ui/form/ui/form-root'
 
 import { Button } from '@/shared/ui/button'
+import { FormRoot } from '@/shared/ui/form/ui/form-root'
 import { Typography } from '@/shared/ui/typography'
 
 type ProfileFormProps = {
